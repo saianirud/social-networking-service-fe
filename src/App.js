0be62dcebf15f43110adf79dde625cbcac52c6ea
Sai Anirud <saianirudh1000@gmail.com>
2021-03-01 09:16:02 +0530

    socialmediafrontend

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Fragment, Component } from 'react';
 import './App.css';
+import { Redirect, Switch } from 'react-router-dom';
+import Auth from './components/Auth/Auth';
+import SnackBar from './shared/SnackBar/SnackBar';
+import Spinner from './shared/Spinner/Spinner';
+import Homepage from './components/Homepage/Homepage';
+import AuthenticatedRoute from './guards/AuthenticatedRoute/AuthenticatedRoute';
+import AuthRoute from './guards/AuthRoute/AuthRoute';
+import * as authActions from './components/Auth/store/auth.actions';
+import { connect } from 'react-redux';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  componentWillMount() {
+    const token = localStorage.getItem('token');
+    if (token) {
+      this.props.autoLogin();
+    }
+  }
+
+  componentDidMount() {
+    console.log('[App.js] componentDidMount');
+  }
+
+  render() {
+    console.log('[App.js] render');
+    return (
+      <Fragment>
+        <SnackBar></SnackBar>
+        <Spinner></Spinner>
+        <Switch>
+          <Redirect exact path="/auth" to="/auth/login" />
+          <Redirect exact path="/" to="/auth/login" />
+          <Redirect exact path="/homepage" to="/homepage/newsfeed" />
+          <Redirect exact path='/homepage/settings' to='/homepage/settings/privacy' />
+          <AuthRoute path="/auth" component={Auth}></AuthRoute>
+          <AuthenticatedRoute path="/homepage" component={Homepage}></AuthenticatedRoute>
+        </Switch>
+      </Fragment>
+    );
+  }
 }
 
-export default App;
+const mapDispatchToProps = dispatch => {
+  return {
+    autoLogin: () => dispatch(authActions.autoLogin())
+  }
+}
+
+export default connect(null, mapDispatchToProps)(App);
